d4c3d012d26cdb7caa2ece1bfae7f7f33fdc92a0
Mostofa-Abedin <shekh.mostofa.abedin@gmail.com>
2024-07-01 16:20:04 +1000

    Main program created. Choices/Options added. Code for options not yet added

---
+++ main.py
@@ -0,0 +1,36 @@
+# Create a function to run the Car Dealership Management System
+def main():
+	# Use a while loop to continiously show options
+    while True:
+        # Display the menu options to the user
+        print("\nCar Welcome to the car Dealership Management System")
+        print("1. Upload to car database")
+        print("2. Search car database")
+        print("3. Append to car database")
+        print("4. Estimate used car price")
+        print("5. Find cars by budget")
+        print("6. Exit")
+
+        # Get the user's choice from the menu
+        choice = input("Choose an option: ")
+
+        if choice == '1':
+            # Option 1: Upload car database
+            
+        elif choice == '2':
+            # Option 2: Search car database
+            
+        elif choice == '3':
+            # Option 3: Append to car database
+            
+        elif choice == '4':
+            # Option 4: Estimate used car price
+            
+        elif choice == '5':
+            
+        elif choice == '6':
+            # Option 6: Exit the application
+           
+        else:
+            # Handle invalid menu choice
+            print("Invalid choice. Please try again.")
